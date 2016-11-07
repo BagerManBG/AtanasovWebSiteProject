06c98e907d064c29d1bb812cc9265c726c1aedc9
VasilZhuhov <vaset0_98@abv.bg>
2016-11-07 21:47:38 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php ?>
+<html lang="en">
+    <head>
+            <meta charset="utf-8">
+            <link href="css/style.css" rel="stylesheet"> 
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+            <link href="css/style.css" rel="stylesheet"> 
+            <link href="css/pgwslider.css" rel="stylesheet"> 
+            <script src="js/jquery.js"></script>
+            <script src="js/pgwslider.js"></script>
+            <script src="js/script.js"></script>
+            
+    </head>
+
+    <body>
+            <ul class="nav-ul">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Q&A</a></li>
+                <li><a href="#">Lessons</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            <div class="parallax"></div>
+            <div class="slideshow">
+                <ul class="pgwSlider">
+                    <li><img src="img/002.jpg" alt="Paris, France" data-description="Eiffel Tower and Champ de Mars"></li>
+                    <li><img src="img/003.jpg" alt="Montréal, QC, Canada" data-large-src="img/003.jpg"></li>
+                    <li>
+                        <img src="img/004.jpg">
+                        <span>Shanghai, China</span>
+                    </li>
+                    <li>
+                            <img src="img/005.jpg">
+                        <span>New York, NY, USA</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>    
+            <div class="parallax_1"></div>
+
+    </body>
