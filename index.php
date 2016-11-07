ba6c144bce44ff64ff896f184b0bc1097a9a0203
VasilZhuhov <vaset0_98@abv.bg>
2016-11-07 23:31:13 +0200

    New Nav

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Q&A</a></li>
                 <li><a href="#">Lessons</a></li>
+                <li><a href="#">Feedback</a></li>
                 <li><a href="#">Login</a></li>
             </ul>
             <div class="parallax"></div>
@@ -37,7 +38,22 @@
                     </li>
                 </ul>
             </div>    
-            <div class="parallax_1"></div>
+            <div class="parallax"></div>
+            <div class="parallax_1">
+                <h2>Hello World</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a eros magna. Proin fermentum, felis id fringilla semper, risus ante sollicitudin ante, ut sagittis dui nisl vitae felis. Maecenas eu laoreet leo. Mauris erat nisi, tristique eu venenatis sed, ultricies non ex. In fermentum nisl nec massa tristique, nec porta lectus tincidunt. Pellentesque tellus dui, placerat ut laoreet et, aliquam vitae erat. Nullam hendrerit dapibus turpis nec iaculis. Aliquam quam felis, imperdiet id est quis, tincidunt luctus nulla. Pellentesque tempor massa ut aliquam viverra. Cras blandit, nunc sit amet laoreet facilisis, tellus nisl consequat nunc, et vulputate urna lacus vitae turpis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras faucibus non ligula in porttitor. Ut eu nulla non arcu interdum tempus. Aenean aliquet ornare erat, et bibendum tortor porta ut.
+
+Mauris at nisl tristique, varius risus sed, sodales ante. Nunc accumsan sapien ut dapibus malesuada. Quisque nisl elit, tempus non porttitor hendrerit, ornare id justo. Vivamus quis aliquet neque. Vestibulum dapibus leo dapibus, fringilla purus sed, tempor metus. Nam pharetra dolor metus, elementum mollis mi dapibus nec. Vivamus ut rhoncus massa, non laoreet dolor. Aliquam erat volutpat. Suspendisse potenti. Morbi non blandit mauris. Duis non lorem ultrices, laoreet elit id, accumsan magna. Maecenas ut diam eget diam posuere viverra. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ac elit quam. Nunc ut auctor dolor.
+
+Praesent commodo elit commodo enim consequat hendrerit. Nullam venenatis lacus odio, vel tincidunt sem blandit vel. Sed sollicitudin a massa dapibus condimentum. Mauris condimentum non enim id commodo. Vestibulum ullamcorper, massa nec ultrices elementum, orci erat tristique turpis, at sagittis magna felis non dolor. Suspendisse at varius est. Cras finibus interdum tristique. Pellentesque maximus tempus elit, eget fermentum erat pulvinar sed. Ut molestie pellentesque eros, congue venenatis velit lobortis sed. Donec congue eleifend enim, ut eleifend nisi porttitor ac.
+
+In accumsan convallis ante at feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non aliquet ex. Morbi sit amet nisi neque. Donec vel fermentum odio, sit amet dapibus orci. Aliquam sit amet aliquam enim, viverra tristique neque. Curabitur porttitor velit quis orci egestas commodo. Nam massa nisi, tincidunt sit amet pretium sit amet, varius ac elit. Morbi sed lacus pulvinar, posuere neque vel, maximus sapien. Nam ac pharetra orci, tincidunt placerat ipsum. Ut sagittis tellus purus, varius congue augue blandit sit amet. Morbi aliquam dictum semper. Ut faucibus et ex nec venenatis.
+
+Nulla finibus tincidunt hendrerit. Donec dapibus eu risus sit amet egestas. Cras commodo dictum lorem, pretium scelerisque mi scelerisque a. Duis porta mattis elit at convallis. Fusce vestibulum tristique purus sit amet faucibus. Donec scelerisque sem nisl, eu molestie ante elementum ac. In porta justo posuere arcu dignissim, eu hendrerit ante tincidunt.</p>
+            </div>                        
+            <div class="parallax"></div>
+                        
+
 
     </body>
 
