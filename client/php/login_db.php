fd15e5281208c4b8e044758eaaeac28cfaf0c84f
nakaTawe <liverpoolnasko@abv.bg>
2016-11-15 00:33:44 +0200

    dddd

--- client/php/login_db.php
+++ client/php/login_db.php
@@ -2,27 +2,27 @@
 require_once "db.class.php";
 $data = $_POST;
 echo "<pre>";
-$data['username_log'] = mysqli_real_escape_string($db->dbHandle, $data['username_log']);
-$query = "SELECT * FROM `users` WHERE `username_sign` = '".$data['username_log']."' ";
+$data['email_log'] = mysqli_real_escape_string($db->dbHandle, $data['email_log']);
+$query = "SELECT * FROM `users` WHERE `email` = '".$data['email_log']."' ";
 $result = $db->fetchArray($query);
 if(!empty($result))
 {
-	if($result[0]['password_sign'] == md5($data['password_log']))
+	if($result[0]['password'] == md5($data['password_log']))
 	{
 		// 
 		$_SESSION['logged_user'] = $result[0];
-		header('Location: ../index.php');
+		header('Location: ../profile.html');
 	}
 	else //if username exists but password is wrong
 	{
-		header('Location: ../index.php');
+		header('Location: ../index.html');
 
 	
 	}
 }
 else //if no user is found
 {
-		header('Location: ../index.php');
+		header('Location: ../index.html');
 
 	
 	
