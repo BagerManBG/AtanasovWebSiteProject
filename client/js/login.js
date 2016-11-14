8c8478c4021a7925a8e571d7d88475460a2f4430
nakaTawe <liverpoolnasko@abv.bg>
2016-11-14 14:45:09 +0200

    ddddd

---
+++ client/js/login.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.positionLeft').click(function(){
+    	$('.regAction').hide()
+    	$('.logAction').show()
+    });
+    $('.positionRight').click(function(){
+    	$('.logAction').hide()
+    	$('.regAction').show() 
+    });
+});
