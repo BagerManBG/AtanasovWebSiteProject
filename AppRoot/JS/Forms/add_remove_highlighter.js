d75b19966358f0c32e5002dd71f716b38e1d3aa3
petar98 <petaryanakiev.py@gmail.com>
2016-11-29 23:59:33 +0200

    Admin chat

--- AppRoot/JS/Forms/add_remove_highlighter.js
+++ AppRoot/JS/Forms/add_remove_highlighter.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
   $('input').bind('keyup focus blur', function(){
-
     var label = $(this).prev('label');
     if ($(this).val() === '') {
       label.removeClass('active highlight');
