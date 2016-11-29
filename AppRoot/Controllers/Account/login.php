48b411ff35b754b80174add67848518cfd96f0ed
nakaTawe <liverpoolnasko@abv.bg>
2016-11-30 00:09:47 +0200

    dddd

---
+++ AppRoot/Controllers/Account/login.php
@@ -0,0 +1,32 @@
+<?php
+require_once "../../Models/Database/db.class.php";
+
+$data = $_POST;
+
+echo "<pre>";
+$data['email_log'] = mysqli_real_escape_string($db->dbHandle, $data['email_log']);
+$query = "SELECT * FROM `users` WHERE `email` = '".$data['email_log']."' ";
+$result = $db->fetchArray($query);
+if(!empty($result))
+{
+	if($result[0]['password'] == md5($data['password_log']))
+	{ 
+		$_SESSION['logged_user'] = $result[0];
+		header('Location: ../index.php');
+		
+	}
+	else //if username exists but password is wrong
+	{
+		header('Location: ../logIn.php');
+		
+
+	
+	}
+}
+else //if no user is found
+{
+		header('Location: ../logIn.php');
+		
+	
+	
+}
