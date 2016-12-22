0b8d766e2c1d51d02538eb1d7f36e46cc5160de1
BagerManBG <toshko_1998@abv.bg>
2016-12-22 23:29:28 +0200

    minor change

--- AppRoot/JS/Forms/add_remove_highlighter.js
+++ AppRoot/JS/Forms/add_remove_highlighter.js
@@ -1,11 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('input').bind('keyup focus blur', function(){
-    var label = $(this).prev('label');
-    if ($(this).val() === '') {
-      label.removeClass('active highlight');
-    } else {
-      label.addClass('active highlight');
-    }
-  });
+    $('input').bind('keyup focus blur', function() {
+
+        var label = $(this).prev('label');
+        var span = label.find('span');
+
+        if ($(this).val() === '') {
+
+            span.show();
+            label.removeClass('active highlight');
+        } else {
+
+            span.hide();
+            label.addClass('active highlight');
+        }
+    });
 });
