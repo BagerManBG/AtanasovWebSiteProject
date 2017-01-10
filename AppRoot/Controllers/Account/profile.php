364d21c1df8bf3a815479561739f334f33855a30
VasilZhuhov <vaset0_98@abv.bg>
2017-01-10 22:56:49 +0200

    Profile Edit

--- AppRoot/Controllers/Account/profile.php
+++ AppRoot/Controllers/Account/profile.php
@@ -9,25 +9,115 @@ require_once "../../Models/Database/db.class.php";
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
+$password =$_POST['password'];
+$password_2 =$_POST['password_2'];
 
-$user_id = $_SESSION['logged_user']['id'];
 
-$q = "SELECT * FROM `users` WHERE `id` = '$user_id'";
 
-$user_data = $db->fetchArray($q);
+    /*if($password =$_POST['password'] == $password_2 =$_POST['password_2'])
+        {
+        $user_id = $_SESSION['logged_user']['id'];
 
-$data = [
-	'id' => $user_data[0]['id'],
-	'first_name' => $first_name,
-	'last_name' => $last_name,
-	'email' => $email
-];
+        $q = "SELECT * FROM `users` WHERE `id` = '$user_id'";
 
-$db->saveArray('users', $data);
+        $user_data = $db->fetchArray($q);
+        if(!empty($password))
+        {
+        $data = [
+            'id' => $user_data[0]['id'],
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $email,
+            'password' => md5($password)
+            
+            ];
+            }
+         else{
+           $data = [
+            'id' => $user_data[0]['id'],
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $email,
+            
+            ];
+         }   
+        $db->saveArray('users', $data);
 
-$_SESSION['logged_user']['first_name'] = $first_name;
-$_SESSION['logged_user']['last_name'] = $last_name;
-$_SESSION['logged_user']['email'] = $email;
+        $_SESSION['logged_user']['first_name'] = $first_name;
+        $_SESSION['logged_user']['last_name'] = $last_name;
+        $_SESSION['logged_user']['email'] = $email;
 
-header('location: ../../index.html');
-?>
+        header('location: ../../index.html');
+        }
+        else{
+
+                 echo"<script language='javascript'>
+                    alert('passwords must matches ')
+        </script>
+        ";*/
+
+                    if(empty($password))
+{
+            $user_id = $_SESSION['logged_user']['id'];
+
+            $q = "SELECT * FROM `users` WHERE `id` = '$user_id'";
+
+            $user_data = $db->fetchArray($q);
+
+            $data = [
+                'id' => $user_data[0]['id'],
+                'first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email
+            ];
+
+            $db->saveArray('users', $data);
+
+            $_SESSION['logged_user']['first_name'] = $first_name;
+            $_SESSION['logged_user']['last_name'] = $last_name;
+            $_SESSION['logged_user']['email'] = $email;
+
+            header('location: ../../index.html');
+            
+           
+
+              
+}
+else {
+
+
+            if($password  == $password_2 )
+        {
+        $user_id = $_SESSION['logged_user']['id'];
+
+        $q = "SELECT * FROM `users` WHERE `id` = '$user_id'";
+
+        $user_data = $db->fetchArray($q);
+
+        $data = [
+            'id' => $user_data[0]['id'],
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $email,
+            'password' => md5($password)
+        ];
+
+        $db->saveArray('users', $data);
+
+        $_SESSION['logged_user']['first_name'] = $first_name;
+        $_SESSION['logged_user']['last_name'] = $last_name;
+        $_SESSION['logged_user']['email'] = $email;
+
+        header('location: ../../index.html');
+        }
+        else{
+
+                 echo"<script language='javascript'>
+                    alert('passwords must matches ')
+        </script>
+        ";
+
+            }
+    }
+        
+        ?>
