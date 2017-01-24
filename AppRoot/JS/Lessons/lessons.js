49a4992c9c1efc0f9199766eab0f093d8875cd5b
VasilZhuhov <vaset0_98@abv.bg>
2017-01-25 00:33:28 +0200

    Lessons

--- AppRoot/JS/Lessons/lessons.js
+++ AppRoot/JS/Lessons/lessons.js
@@ -1,9 +1,17 @@
-$(function(){
+$( document ).ready(function() {
+
   var $menuitems = $("#menuitems"); 
   $menuitems.css("display:none");
   $("#heading").click(function(){
     $("#menuitems").slideToggle('200');
-  });
+
 });
 
+  var $menuitems_1 = $("#menuitems_1"); 
+  $menuitems_1.css("display:none");
+  $("#headin_1").click(function(){
+    $("#menuitems_1").slideToggle('200');
+  });
+
+});
 
