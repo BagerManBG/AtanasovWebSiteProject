9e3c882dafc97378003abd7c1eff9044bb436db2
TheAbraxas98 <nekron_warrior@live.com>
2017-01-27 12:40:51 +0200

    Дизаааайн

--- AppRoot/Controllers/lectures_controller.php
+++ AppRoot/Controllers/lectures_controller.php
@@ -1,6 +1,5 @@
 <?php
   require_once "../Models/Database/db.class.php";
-  require_once "Account/check_role.php";
   $data = $_POST;
   $method = $_POST["action"];
   $controller = new LecturesController($db);
